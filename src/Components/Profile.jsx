85e279cc0f535d538551d0d68f7fef6b4c7accc4
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2024-07-15 15:41:08 +0530

    Update tooltip texts

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -28,8 +28,8 @@ export const Profile = ({
           <span className="social__link print">
             <i className="bx bx-map social__icon" /> {location}
           </span>
-          <BoxIcon className="home__icon bx-envelope" label={`Enviar un correo electrónico a ${name}`} url={`mailto:${email}`} />
-          <BoxIcon className="home__icon bx-phone" label={`Llamar por teléfono a ${name}`} url={`tel:${telephone}`} />
+          <BoxIcon className="home__icon bx-envelope" label={`Write an email to ${name}`} url={`mailto:${email}`} />
+          <BoxIcon className="home__icon bx-phone" label={`Dial ${name}'s phone`} url={`tel:${telephone}`} />
           {true && social.map((social) => <BoxIcon key={social.name} {...social} />)}
         </div>
       </div>
